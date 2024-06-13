0ea7c3eb298d4f423ed0cb6b711c9b7cf22216c2
heqat <arturgouveia21@gmail.com>
2024-06-13 12:35:22 -0300

    Faixa

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "de57026d0da7b512"; }
+/******/ 		__webpack_require__.h = function() { return "7021be990b3d3ace"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
